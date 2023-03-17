e94b798691221984d467f3b5847fe778fc40e3fc
snehaljadhav23 <127172089+snehaljadhav23@users.noreply.github.com>
2023-03-17 19:41:32 +0530

    Add files via upload

---
+++ Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Emp_Id]   INT        NOT NULL,
+    [Emp_Name] NCHAR (10) NOT NULL,
+    [Address]  NCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Emp_Id] ASC)
+);
+
---
+++ Training.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Training] (
+    [Emp_Id]       INT        NOT NULL,
+    [TrainingType] NCHAR (10) NOT NULL,
+    [TrainerName]  NCHAR (10) NOT NULL,
+    [StartDate]    DATE       NOT NULL,
+    [EndDate]      DATE       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Emp_Id] ASC)
+);
+
